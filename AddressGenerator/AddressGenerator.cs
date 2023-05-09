58e5a372ba9a62e91d8522c866dc03d2bff7a1df
Michael Mantoan <mantoan.michael@iisviolamarchesini.edu.it>
2023-05-09 19:00:54 +0200

    Aggiungere i file di progetto.

---
+++ AddressGenerator/AddressGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressGenerator
+{
+    internal class AddressGenerator : IAddress
+    {
+        public string generateIPV4()
+        {
+            return "";
+        }
+        public string generateSubnet()
+        {
+            return "";
+        }
+    }
+}
---
+++ AddressGenerator/IAddress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressGenerator
+{
+    internal interface IAddress
+    {
+        string generateIPV4();
+        string generateSubnet();
+    }
+}
