78a5fc3dc305ba27604b67661390eed00d3da0cf
Michael Mantoan <mantoan.michael@iisviolamarchesini.edu.it>
2023-05-10 18:43:54 +0200

    test unitario corretto uso dei metodi già presenti date dalla classe IPAddress con test unitario positivo e uso dello shift

--- AddressGenerator/AddressGenerator.cs
+++ AddressGenerator/AddressGenerator.cs
@@ -1,20 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AddressGenerator
 {
-    internal class AddressGenerator : IAddress
+    public class AddressGenerator : IAddress
     {
+         int[] _bits;
+        int _bitssubnet;
+
+        public AddressGenerator(int[] bits, int subnetbit)
+        {
+            if (bits.Length != 32)
+            {
+                throw new ArgumentException("Il numero di bit deve essere esattamente 32.");
+            }
+
+            _bits = bits;
+            _bitssubnet = subnetbit;
+        }
+
         public string generateIPV4()
         {
-            return "";
+            byte[] bytes = new byte[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                int startIndex = i * 8;
+                int endIndex = startIndex + 8;
+                int decimalValue = Convert.ToInt32(string.Join("", _bits.Skip(startIndex).Take(8)), 2);
+                bytes[i] = Convert.ToByte(decimalValue);
+            }
+
+            return new IPAddress(bytes).ToString();
         }
+
+        private string GetSubnetMask()
+        {
+
+            uint subnetMask = 0xFFFFFFFF << (32 - _bitssubnet);
+            subnetMask = (uint)IPAddress.HostToNetworkOrder((int)subnetMask);
+            return new IPAddress(subnetMask).ToString();
+        }
+
         public string generateSubnet()
         {
-            return "";
+            string subnetMask = GetSubnetMask();
+            return subnetMask;
         }
+
+
+
+
     }
 }
---
+++ AddressGeneratorTests/AddressGeneratorTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AddressGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressGenerator.Tests
+{
+    [TestClass()]
+    public class AddressGeneratorTests
+    {
+        [TestMethod]
+        public void TestGenerateIPv4()
+        {
+            // Arrange
+            int[] bits = { 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0 };
+            int subnetBits = 18;
+            AddressGenerator addressGenerator = new AddressGenerator(bits , subnetBits);
+
+            // Act
+            string ipv4 = addressGenerator.generateIPV4();
+
+            // Assert
+            Assert.AreEqual("192.168.206.4", ipv4);
+        }
+
+        [TestMethod]
+        public void TestGenerateSubnet()
+        {
+            // Arrange
+            int[] bits = { 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0 };
+            int subnetBits = 18;
+            AddressGenerator addressGenerator = new AddressGenerator(bits, subnetBits);
+
+            // Act
+            string subnet = addressGenerator.generateSubnet();
+
+            // Assert
+            Assert.AreEqual("255.255.192.0", subnet);
+        }
+    }
+}
